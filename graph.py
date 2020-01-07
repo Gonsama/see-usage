cc6f18982fc9476134cdf83fea4113b19290fd71
Semih Locqueneux <semih.locqueneux@hotmail.com>
2020-01-07 15:06:23 +0100

    reusable functions put in utils.py + addition of different type of data to plot

--- graph.py
+++ graph.py
@@ -1,83 +1,23 @@
-import csv
+import utils
 import argparse
 import os
 from fnmatch import fnmatch
 import matplotlib.pyplot as plt
 import matplotlib.dates as md
 import requests
-import json
 import re
 import random
 from datetime import datetime
 
 
-def get_timestamp_from_json(json_data):
-    """return timestamp of library specified by json data"""
-    #format is dict containing 2 keys response and responseheader, the values are also dict
-    response_data = json_data["response"]
-    docs_data = response_data["docs"]
-    #if no timestamp
-    if len(docs_data) == 0:
-        return 0
-    else:
-        return docs_data[0]["timestamp"]
-
-def get_timestamp(path):
-    """return timestamp of library specified by path"""
-
-    #look for properties file first, if it doesn't exist request REST api for the properties
-    if os.path.isfile(path + "/properties.json"):
-        print (path + "/properties.json exist")
-        with open(path + "/properties.json") as json_file:
-            json_data = json.load(json_file)
-            return get_timestamp_from_json(json_data)
-    else:
-        print (path + "/properties.json do not exist" )
-
-
-    #properties file doesnt exist
-
-    # example of split path : ['csv-data', 'commons-cli', 'commons-cli', '1.3']
-    split_path = re.split(':|/',path) 
-
-    url = 'https://search.maven.org/solrsearch/select?q=g:"' + split_path[1] + '"%20AND%20a:"' + split_path[2] + '"%20AND%20v:"' + split_path[3] + '"&wt=json'
-
-    print ("Requesting properties from REST api ...")    
-
-    try:
-        myResponse = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        print (e)
-        return 0
-
-    # For successful API call, response code will be 200 (OK)
-    if(myResponse.ok):
-
-        print ("Response to get properties of " + path + " is OK")
-
-        # Loading the response data into a dict variable
-        # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
-        json_data = json.loads(myResponse.content)
-        
-        #write data in file for next times
-        with open( path + '/properties.json', 'w') as outfile:
-            json.dump(json_data, outfile)        
-
-        return get_timestamp_from_json(json_data)
-    else:
-        # If response code is not ok (200)
-        print ("Response to get properties of " + path + " is not OK")
-        myResponse.raise_for_status()
-        return 0 
-
 def remove_library(index):
     """remove library from all dicts and lists related to the final plot"""
     global versions_tuple
-    global reuse_core_sizes
+    global y_data
     global versions
     versions_tuple.pop(index)
     versions.pop(index)
-    for key,value in reuse_core_sizes.items():
+    for key,value in y_data.items():
         value.pop(index)
 
 ########################
@@ -87,6 +27,7 @@ def remove_library(index):
 parser = argparse.ArgumentParser(description='Welcome!')
 parser.add_argument("--lp", required=True, type=str, help="path to the repertory of the library (groupid + artifactid)")
 parser.add_argument("--p", nargs="+", type=int, help="Reuse-core percent values we want in plot (1 or more values)")
+parser.add_argument("--type",type=int, choices={1, 2}, default = 1, help="Type of plot: 1.evolution of reuse-core-size; 2.evolution of ratio reuse-core-size/total-used-api-size")
 parser.add_argument('--sot', default=False, action='store_true', help="Space between versions (x-axis) scales according to time between them. Default behaviour is equal space between each versions")
 parser.add_argument("--o", default="reuse-core", type=str, help="file name for the output png")
 parser.add_argument("--regex", default = "a^", type=str, help="regex defining the versions that we don't want to see on the graph")
@@ -100,28 +41,31 @@ root_directory = args.lp
 
 #getting all versions (path, timestamp) tuple that are not matching regex
 regex = re.compile( args.regex)
-versions_tuple= [(x[0],get_timestamp(x[0])) for x in os.walk(args.lp) if x[0] != root_directory and not regex.search(x[0])]
+versions_tuple= [(x[0],utils.get_timestamp(x[0])) for x in os.walk(args.lp) if x[0] != root_directory and not regex.search(x[0]) and utils.get_csv_rows_nb(x[0] + "/library-usage.csv") != 0]
 
-reuse_core_sizes = {}
+y_data = {}
 #init dict key = reuse-core percent, value = list of sizes
 for percent in args.p:
-    reuse_core_sizes[percent] = []
+    y_data[percent] = []
 
 #sort list so that versions are in ascending order
 versions_tuple = sorted(versions_tuple, key=lambda tup: tup[1])
 
-#visit subdirectories one by one to compute the reuse-core sizes
-for path,t in versions_tuple:
-    for percent in args.p:
-        try:
-            f = open(path + "/reuse-core-" + str(percent) + ".csv", "r")
-            #minus 1 bcs first line is column names        
-            size = sum(1 for line in f) - 1
-            reuse_core_sizes[percent].append(size)
-            f.close()
-        except IOError:
-            print ("File " + path + "/reuse-core-" + str(percent) + ".csv" + " do not exist. You should use the script to get the reuse-core before proceeding. ")
-            exit()
+#visit subdirectories one by one to compute the wanted values according to type argument
+if args.type == 1:
+    for path,t in versions_tuple:
+        for percent in args.p:
+            size = utils.get_csv_rows_nb(path + "/reuse-core-" + str(percent) + ".csv")
+            y_data[percent].append(size)
+    y_axis_title = "reuse-core-size"
+elif args.type == 2:
+    for path,t in versions_tuple:
+        for percent in args.p:
+            reuse_core_size = utils.get_csv_rows_nb(path + "/reuse-core-" + str(percent) + ".csv")
+            total_used_api_size = utils.get_unique_used_members(path + "/library-usage.csv")
+            ratio = reuse_core_size / float(total_used_api_size)
+            y_data[percent].append(ratio)
+    y_axis_title = "RATIO reuse-core-size / total-used-api-members"
 
 #x-axis of plot will be different according to sot argument
 versions = [path.split("/")[2] for path,t in versions_tuple]
@@ -166,13 +110,13 @@ if args.sot:
 
 # multiple line plot
 num=0
-for percent,sizes in reuse_core_sizes.items():
+for percent,sizes in y_data.items():
     num+=1
     plt.plot(x_axis, sizes, marker='o', color=palette(num), linewidth=1, alpha=0.9, label="p = " + str(percent))
 
 #if x_axis is time, we have to put the library version to at teast one set of points (random set)
 if args.sot:
-    random_value_from_dict = random.choice(list(reuse_core_sizes.values()))
+    random_value_from_dict = random.choice(list(y_data.values()))
     for i in range(0,len(x_axis)): 
         plt.text(x_axis[i], random_value_from_dict[i],  versions[i], fontsize=9)
  
@@ -186,6 +130,6 @@ plt.tight_layout()
 #titles
 plt.title(root_directory, loc='center', fontsize=24, fontweight=0, color='red')
 plt.xlabel(x_axis_title)
-plt.ylabel("reuse-core size")
+plt.ylabel(y_axis_title)
 plt.savefig(args.o + '.png')
 plt.show()
---
+++ utils.py
@@ -0,0 +1,91 @@
+import os
+import json
+import csv
+
+def get_csv_rows_nb(path):
+    """return number of rows of a file whose path is given"""
+    try:
+        f = open(path, "r")
+        #minus 1 bcs first line is column names        
+        size = sum(1 for line in f) - 1
+        f.close()
+        return size
+    except IOError:
+        print ("File " + path + " do not exist. You should use the script to get the reuse-core before proceeding. ")
+        exit()
+
+def get_unique_used_members(path):
+    """return number of unique api_members in file whose path is given"""
+    try:
+        with open(path, 'r') as f:
+            reader = csv.reader(f)
+            csv_data = list(reader) 
+            #first row must be deleted because it represents the column names
+            csv_data.pop(0)      
+            api_members = [x[3] + ":" + x[4] + ":" + x[5] for x in csv_data]
+            unique_api_members = set(api_members)           
+            f.close()
+            return len(unique_api_members)
+    except IOError:
+        print ("File " + path + " do not exist. You should use the script to export library usage before proceeding. ")
+        exit()
+
+def get_timestamp_from_json(json_data):
+    """return timestamp of library specified by json data"""
+    #format is dict containing 2 keys response and responseheader, the values are also dict
+    response_data = json_data["response"]
+    docs_data = response_data["docs"]
+    #if no timestamp
+    if len(docs_data) == 0:
+        return 0
+    else:
+        return docs_data[0]["timestamp"]
+
+def get_timestamp(path):
+    """return timestamp of library specified by path"""
+
+    #look for properties file first, if it doesn't exist request REST api for the properties
+    if os.path.isfile(path + "/properties.json"):
+        print (path + "/properties.json exist")
+        with open(path + "/properties.json") as json_file:
+            json_data = json.load(json_file)
+            return get_timestamp_from_json(json_data)
+    else:
+        print (path + "/properties.json do not exist" )
+
+
+    #properties file doesnt exist
+
+    # example of split path : ['csv-data', 'commons-cli', 'commons-cli', '1.3']
+    split_path = re.split(':|/',path) 
+
+    url = 'https://search.maven.org/solrsearch/select?q=g:"' + split_path[1] + '"%20AND%20a:"' + split_path[2] + '"%20AND%20v:"' + split_path[3] + '"&wt=json'
+
+    print ("Requesting properties from REST api ...")    
+
+    try:
+        myResponse = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        print (e)
+        return 0
+
+    # For successful API call, response code will be 200 (OK)
+    if(myResponse.ok):
+
+        print ("Response to get properties of " + path + " is OK")
+
+        # Loading the response data into a dict variable
+        # Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+        json_data = json.loads(myResponse.content)
+        
+        #write data in file for next times
+        with open( path + '/properties.json', 'w') as outfile:
+            json.dump(json_data, outfile)        
+
+        return get_timestamp_from_json(json_data)
+    else:
+        # If response code is not ok (200)
+        print ("Response to get properties of " + path + " is not OK")
+        myResponse.raise_for_status()
+        return 0 
+
